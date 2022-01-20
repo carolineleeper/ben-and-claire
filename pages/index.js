ccc8d652490e2b7d4abe5f29c65dd3671e6c9eb4
Caroline Leeper <caroline.leeper@gmail.com>
2022-01-20 21:53:14 +1100

    fix linting

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
+const convincingText = `Oh, hello! We didn't see you there! Would you like a compiled collection of reasons you
+				should move to the hills? Sure, here you go! As a quick summary, You'll be closer to
+				Healsville sanctuary, Cape Paterson, the Yarra Valley, Warburton, the snow, and your
+				precious precious family. We can baby sit! Nanny and Poppy can babysit! You basically won't
+				have to parent ever again!`;
+
 export default function Home() {
 	return (
 		<div className={styles.container}>
@@ -12,13 +18,7 @@ export default function Home() {
 				/>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-			<p className={styles.description}>
-				Oh, hello! We didn't see you there! Would you like a compiled collection of reasons you
-				should move to the hills? Sure, here you go! As a quick summary, You'll be closer to
-				Healsville sanctuary, Cape Paterson, the Yarra Valley, Warburton, the snow, and your
-				precious precious family. We can baby sit! Nanny and Poppy can babysit! You basically won't
-				have to parent ever again!
-			</p>
+			<p className={styles.description}>{convincingText}</p>
 		</div>
 	);
 }
